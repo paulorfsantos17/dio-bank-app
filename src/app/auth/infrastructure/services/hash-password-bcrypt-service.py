aad8ab7e9a3c235c8c130d08833371457963c704
Paulo Santos <paulorfsantos17@gmail.com>
2025-08-03 17:27:50 -0300

    feat: add service bcrypt

---
+++ src/app/auth/infrastructure/services/hash-password-bcrypt-service.py
@@ -0,0 +1,13 @@
+from bcrypt import bcrypt
+
+from src.app.auth.domain.services.hash_password_service import HashPasswordService
+
+
+class HashPasswordBCryptdService(HashPasswordService):
+  def hash_password(self, plain_password: str) -> str:
+    password_hash = bcrypt.hashpw(plain_password.encode('utf-8'), bcrypt.gensalt())
+    return password_hash
+  
+  def verify_password(self, plain_password, hashed_password):
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+  
